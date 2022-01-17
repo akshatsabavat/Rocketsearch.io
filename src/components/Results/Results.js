e01fb461bb66c2a8f5fa0770f869562fa261e72f
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-01-17 21:59:53 +0530

    feat(Home --Results) - Configured Results with Loader component and loading gaurd clause

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -5,10 +5,13 @@ import ReactPlayer from "react-player";
 import { useResultContext } from "../../contexts/ResultContextProvider";
 
 import "../Results/Results.scss";
+import LoadingComponent from "../LoadingComponent/LoadingComponent";
 
 export default function Results() {
   const { getResults, results, searchTerm, setSearchTerm, Loading } =
     useResultContext(); //constum built hook for getting result contexts
   const location = useLocation();
+
+  if (Loading) return <LoadingComponent />; // gaurd clause to ensure the loader is rendered during the process of the API call
   return <div classname="SearchResultsComponent">Results</div>;
 }
