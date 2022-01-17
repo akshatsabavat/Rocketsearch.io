2e541ab5fd232a5b84a87f6b5add6ec0952a147c
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-01-17 11:32:15 +0530

    feat(Home --Navbar) - initial layout compelete

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../Navbar/Navbar.scss";
+import Search from "../Search/Search";
 
 export default function Navbar() {
   return (
@@ -8,6 +9,7 @@ export default function Navbar() {
       <Link className="NavigationComponent__Link" to="/">
         <p className="NavigationComponent__Link__Logo">Rocketsearch.io</p>
       </Link>
+      <Search />
       <button className="NavigationComponent__ThemeButton">Dark</button>
     </div>
   );
