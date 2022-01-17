a31f01e695e85f5fa05fe1751fa60335ef30402f
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-01-17 11:45:41 +0530

    feat(Home --Results) - Basic JSX and files added

---
+++ src/components/Results/Results.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../Results/Results.scss";
+
+export default function Results() {
+  return (
+    <div classname="SearchResultsComponent">
+      <h1>Results</h1>
+    </div>
+  );
+}
