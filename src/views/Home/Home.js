3a35c36b8a5071fc107e91404b5a79586c720c37
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-01-17 10:33:40 +0530

    feat(Project Template) - code clean up and project setup compelete

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,10 +1,5 @@
-import logo from "../../logo.svg";
 import "./Home.scss";
 
 export default function Home() {
-  return (
-    <div className="App">
-      App
-    </div>
-  );
+  return <div className="App">Not App</div>;
 }
