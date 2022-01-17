0a33e9291fd3f07b4f69b47cde58eabeab3186ff
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-01-17 20:34:48 +0530

    feat(Home --ResultContextProvider) - Basic JSX along with API endpoint  added

---
+++ src/contexts/ResultContextProvider/ResultContextProvider.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { createContext } from "react";
+import { useContext, useState } from "react";
+
+const ResultContext = createContext();
+const baseUrl = "https://google-search3.p.rapidapi.com/api/v1";
+
+export default function ResultContextProvider() {}
