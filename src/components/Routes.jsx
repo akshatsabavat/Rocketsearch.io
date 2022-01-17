69daf8d4db0a2a95552e6f809af4e15b0b601fb1
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-01-17 10:47:23 +0530

    feat(Project Template) - File structure finalized

---
+++ src/components/Routes.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Routes() {
+  return (
+    <div>
+      <h1>Routes</h1>
+    </div>
+  );
+}
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,5 +1,14 @@
 import "./Home.scss";
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
+import Routes from "../../components/Routes";
 
 export default function Home() {
-  return <div className="App">Not App</div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Footer />
+      <Routes />
+    </div>
+  );
 }
