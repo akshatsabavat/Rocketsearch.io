9fe88804ba7c762a354d7597443ff142dba095db
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-01-17 22:44:20 +0530

    feat(Home --Results) - UrlType testing employed with all UrlTypes

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -9,15 +9,23 @@ import LoadingComponent from "../LoadingComponent/LoadingComponent";
 export default function Results() {
   const { getResults, results, searchTerm, setSearchTerm, Loading } =
     useResultContext(); //constum built hook for getting result contexts
-  const location = useLocation();
+  const location = useLocation(); //ensures to store the current url extention to check the current page navigation
 
   console.log(results);
   if (Loading) return <LoadingComponent />; // gaurd clause to ensure the loader is rendered during the process of the API call
   console.log(location.pathname);
 
-  switch (location.pathname) {
+  switch (
+    location.pathname //switch case to changed and render the ResultContext provider depending on the url extention type
+  ) {
     case "/search":
-      return "Search";
+      return "All search results displayed";
+    case "/videos":
+      return "Only video results displayed";
+    case "/news":
+      return "Only news results displayed";
+    case "/images":
+      return "Only imaged results displayed";
     default:
       return "Error";
   }
