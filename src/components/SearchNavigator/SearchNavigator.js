24da985f07ecac9998c41d0bc03bb8d217f1ebe5
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-01-18 16:01:47 +0530

    feat(Home --SearchNavigator) - Basic JSX and files added

---
+++ src/components/SearchNavigator/SearchNavigator.js
@@ -0,0 +1,11 @@
+import "../SearchNavigator/SearchNavigator.scss";
+
+import React from "react";
+
+export default function SearchNavigator() {
+  return (
+    <div className="SearchNavigatorComponent">
+      <h1>SearchNavigator</h1>
+    </div>
+  );
+}
