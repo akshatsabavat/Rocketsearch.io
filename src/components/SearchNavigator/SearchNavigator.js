015d257e2ac280bb9f1e9b820afe6c64a8ab8b8a
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-01-19 11:33:59 +0530

    feat(Home --SearchNavigator) - JSX and Scss updated

--- src/components/SearchNavigator/SearchNavigator.js
+++ src/components/SearchNavigator/SearchNavigator.js
@@ -12,40 +12,52 @@ export default function SearchNavigator() {
     <div className="SearchNavigatorComponent">
       <ul className="SearchNavigatorComponent__Btns">
         <l1>
-          <Link to={`/search`} className="SearchNavigatorComponent__Btns__Item">
+          <Link
+            to={`/search`}
+            className="SearchNavigatorComponent__Btns__Item altItem"
+          >
             All{" "}
             <img
-              className="SearchNavigatorComponent__Btns__Item__ItemIcon"
+              className="SearchNavigatorComponent__Btns__Item__ItemIcon altItem__icon"
               alt="AllIcon"
               src={SearchIcon}
             ></img>
           </Link>
         </l1>
         <l1>
-          <Link to={`/news`} className="SearchNavigatorComponent__Btns__Item">
+          <Link
+            to={`/news`}
+            className="SearchNavigatorComponent__Btns__Item altItem"
+          >
             News
             <img
-              className="SearchNavigatorComponent__Btns__Item__ItemIcon"
+              className="SearchNavigatorComponent__Btns__Item__ItemIcon altItem__icon"
               alt="NewsIcon"
               src={NewsIcon}
             ></img>
           </Link>
         </l1>
         <l1>
-          <Link to={`/images`} className="SearchNavigatorComponent__Btns__Item">
+          <Link
+            to={`/images`}
+            className="SearchNavigatorComponent__Btns__Item altItem"
+          >
             Images
             <img
-              className="SearchNavigatorComponent__Btns__Item__ItemIcon"
+              className="SearchNavigatorComponent__Btns__Item__ItemIcon altItem__icon"
               alt="ImageIcon"
               src={ImagesIcon}
             ></img>
           </Link>
         </l1>
         <l1>
-          <Link to={`/videos`} className="SearchNavigatorComponent__Btns__Item">
+          <Link
+            to={`/videos`}
+            className="SearchNavigatorComponent__Btns__Item altItem"
+          >
             Videos
             <img
-              className="SearchNavigatorComponent__Btns__Item__ItemIcon"
+              className="SearchNavigatorComponent__Btns__Item__ItemIcon altItem__icon"
               alt="VideoIcon"
               src={VideoIcon}
             ></img>
