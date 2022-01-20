cb7653f9699fddcac7a596eed4e896849365f547
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-01-20 21:32:49 +0530

    feat(Home --Search) - Test JSX written

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -4,12 +4,20 @@ import { useState } from "react";
 import "../Search/Search.scss";
 
 export default function Search() {
+  const [searchValue, setSearchValue] = useState("");
+  function beginSearch(event) {
+    setSearchValue(event.target.value);
+    console.log(searchValue);
+  }
   return (
     <div className="SearchComponent">
       <input
         className="SearchComponent__InputFeild"
         type="text"
         placeHolder="Launch in T-minus 10.."
+        onChange={(e) => {
+          beginSearch(e);
+        }}
       />
       <button className="SearchComponent__GoBtn">Go</button>
     </div>
