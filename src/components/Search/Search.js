efa838a13684855b5cff198f47493680e9a20a60
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-01-20 21:55:40 +0530

    feat(Home --SearchBar) - basic JSX and functionality added

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -5,9 +5,11 @@ import "../Search/Search.scss";
 
 export default function Search() {
   const [searchValue, setSearchValue] = useState("");
+  const [buttonState, setButtonState] = useState(0);
+
   function beginSearch(event) {
     setSearchValue(event.target.value);
-    console.log(searchValue);
+    alert(searchValue);
   }
   return (
     <div className="SearchComponent">
@@ -15,8 +17,13 @@ export default function Search() {
         className="SearchComponent__InputFeild"
         type="text"
         placeHolder="Launch in T-minus 10.."
-        onChange={(e) => {
-          beginSearch(e);
+        // onChange={(e) => {
+        //   this.setState({ query: e.target.value });
+        // }}
+        onKeyPress={(e) => {
+          if (e.key === "Enter") {
+            beginSearch(e);
+          }
         }}
       />
       <button className="SearchComponent__GoBtn">Go</button>
