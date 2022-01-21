6c73a3eded82c7155fff3765685904de5114ca4d
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-01-21 19:57:28 +0530

    feat(Home --Search) - Reset JSX

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,32 +1,17 @@
 import React from "react";
+import { useEffect } from "react";
 import { useState } from "react";
 import { useResultContext } from "../../contexts/ResultContextProvider/ResultContextProvider";
+import { useDebounce } from "use-debounce";
 import "../Search/Search.scss";
 
 export default function Search() {
-  const { setSearchTerm, searchTerm } = useResultContext();
-  const [searchValue, setSearchValue] = useState("");
-  const [buttonState, setButtonState] = useState(0);
-
-  function beginSearch(event) {
-    setSearchValue(event.target.value);
-    setSearchTerm(searchValue);
-    console.log(searchTerm);
-  }
   return (
     <div className="SearchComponent">
       <input
         className="SearchComponent__InputFeild"
         type="text"
         placeHolder="Launch in T-minus 10.."
-        // onChange={(e) => {
-        //   this.setState({ query: e.target.value });
-        // }}
-        onKeyPress={(e) => {
-          if (e.key === "Enter") {
-            beginSearch(e);
-          }
-        }}
       />
       <button className="SearchComponent__GoBtn">Go</button>
     </div>
