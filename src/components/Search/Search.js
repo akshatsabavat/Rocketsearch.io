331ddef9af5dd7bd9912ffa2dac9b3376ead6f1f
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-01-21 20:18:37 +0530

    feat(Home --Search) - Go Btn functionality in progress

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -7,15 +7,25 @@ import "../Search/Search.scss";
 
 export default function Search() {
   const [userIP, setuserIP] = useState("Covid 19");
+  const [goBtn, setGoBtn] = useState(false);
   const { setSearchTerm } = useResultContext();
   const [debounceValue] = useDebounce(userIP, 500); // waits for 500ms and then processes the useIP state feild
-
+  const [searchDebounceValue] = useDebounce(userIP, goBtn);
+  function turnSearchOn() {
+    setGoBtn(true);
+    console.log(goBtn);
+  }
   useEffect(() => {
     if (debounceValue) {
       setSearchTerm(debounceValue);
       console.log(userIP);
     }
   }, [debounceValue]); //if any changes in debounce value updates the useResultcontext hook feild setSearch Term
+
+  useEffect(() => {
+    goBtn === true ? setGoBtn(false) : setGoBtn(true);
+    console.log();
+  }, [goBtn]);
   return (
     <div className="SearchComponent">
       <input
@@ -25,7 +35,9 @@ export default function Search() {
         placeHolder="Launch in T-minus 10.."
         onChange={(e) => setuserIP(e.target.value)}
       />
-      <button className="SearchComponent__GoBtn">Go</button>
+      <button onClick={() => turnSearchOn()} className="SearchComponent__GoBtn">
+        Go
+      </button>
     </div>
   );
 }
